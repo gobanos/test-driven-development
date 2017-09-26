285954147e512909d102b454e399a4ae6f268c75
Grégory OBANOS <gregory.obanos@gmail.com>
2017-09-26 08:33:30 +0200

    the money example, chapters 1-6

---
+++ the-money-example/src/lib.rs
@@ -0,0 +1,76 @@
+use std::ops;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Money {
+    amount: i32,
+    currency: Currency,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Currency {
+    Dollar,
+    Franc,
+}
+
+impl Money {
+    pub fn dollar(amount: i32) -> Money {
+        Money {
+            amount,
+            currency: Currency::Dollar,
+        }
+    }
+
+    pub fn franc(amount: i32) -> Money {
+        Money {
+            amount,
+            currency: Currency::Franc,
+        }
+    }
+}
+
+impl ops::Mul<i32> for Money {
+    type Output = Money;
+
+    fn mul(self, multiplier: i32) -> Self::Output {
+        Money {
+            amount: self.amount * multiplier,
+            ..self
+        }
+    }
+}
+
+impl PartialEq for Money {
+    fn eq(&self, other: &Money) -> bool {
+        self.amount == other.amount
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiplication() {
+        let five = Money::dollar(5);
+
+        assert_eq!(five * 2, Money::dollar(10));
+        assert_eq!(five * 3, Money::dollar(15));
+    }
+
+    #[test]
+    fn test_equality() {
+        assert!(Money::dollar(5) == Money::dollar(5));
+        assert!(Money::dollar(5) != Money::dollar(6));
+
+        assert!(Money::franc(5) == Money::franc(5));
+        assert!(Money::franc(5) != Money::franc(6));
+    }
+
+    #[test]
+    fn test_franc_multiplication() {
+        let five = Money::franc(5);
+
+        assert_eq!(five * 2, Money::franc(10));
+        assert_eq!(five * 3, Money::franc(15));
+    }
+}
