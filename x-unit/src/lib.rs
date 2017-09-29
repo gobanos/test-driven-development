fb37f64bc84afe9ca506ea2c18cde37928531a9b
Gregory Obanos <gregory.obanos@gmail.com>
2017-09-30 01:40:02 +0200

    Python version of chapter 19

--- x-unit/src/lib.rs
+++ x-unit/src/lib.rs
@@ -13,18 +13,41 @@ impl From<bool> for TestValue {
 
 pub type TestData = HashMap<String, TestValue>;
 
-pub struct TestCase<F: Fn(&mut TestData) -> ()> {
-    test_data: TestData,
-    test_method: F,
+pub struct TestCase<S, F> where
+    S: Fn(&mut TestData) -> (),
+    F: Fn(&TestData, &mut TestData) -> (),
+{
+    pub setup: S,
+    pub run: F,
 }
 
-impl<F: Fn(&mut TestData) -> ()> TestCase<F> {
-    pub fn new(test_method: F) -> Self {
-        TestCase { test_data: TestData::new(), test_method }
+pub struct TestRunner<S, F> where
+    S: Fn(&mut TestData) -> (),
+    F: Fn(&TestData, &mut TestData) -> (),
+{
+    test_setup: TestData,
+    test_data: TestData,
+    test: TestCase<S, F>,
+}
+
+impl<S, F> TestRunner<S, F> where
+    S: Fn(&mut TestData) -> (),
+    F: Fn(&TestData, &mut TestData) -> (),
+{
+    pub fn new(test: TestCase<S, F>) -> Self {
+        TestRunner {
+            test_setup: TestData::new(),
+            test_data: TestData::new(),
+            test,
+        }
+    }
+
+    pub fn setup(&mut self) {
+        (self.test.setup)(&mut self.test_setup);
     }
 
     pub fn run(&mut self) {
-        (self.test_method)(&mut self.test_data);
+        (self.test.run)(&self.test_setup, &mut self.test_data);
     }
 
     pub fn get(&self, key: &str) -> Option<&TestValue> {
--- x-unit/src/main.rs
+++ x-unit/src/main.rs
@@ -3,15 +3,15 @@ extern crate xunit;
 use xunit::*;
 
 fn main() {
-    TestCase::new(test_case_test).run();
+    TestRunner::new(TestCase { setup: |_| {}, run: test_case_test }).run();
 }
 
-fn was_run(test_data: &mut TestData) {
+fn was_run(_: &TestData, test_data: &mut TestData) {
     test_data.insert("was_run".into(), true.into());
 }
 
-fn test_case_test(_: &mut TestData) {
-    let mut test = TestCase::new(was_run);
+fn test_case_test(_: &TestData, _: &mut TestData) {
+    let mut test = TestRunner::new(TestCase { setup: |_| {}, run: was_run });
     assert_eq!(test.get("was_run"), None);
     test.run();
     assert_eq!(test.get("was_run"), Some(&true.into()));
